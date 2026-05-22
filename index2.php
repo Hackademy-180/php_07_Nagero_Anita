8624798a5ba731ff9ac3999ccc2775a6ef4b48ca
anita <nageroa6@gmail.com>
2026-05-22 22:55:21 +0200

    modificati files index2 e psw.php

--- index2.php
+++ index2.php
@@ -1,60 +1,18 @@
 <?php
+require('index.php');
+
+$valida = false;
+
+while ($valida == false){
 
 
+$password = readline("Inserisci la password: \n");
 
+//struttura di controllo aggiunta alla verifica dei messaggi;
+if(result($password)){
+    $valida = true;
 
-function checkLenght($password) {
-    return strlen($password) >= 8;
+}else{
+    echo "Riprova! \n";
 }
-
-function checkNumber($password) {
-    for ($i = 0; $i < strlen($password); $i++) {
-        if (is_numeric($password[$i])) {
-            return true;
-        }
-    }
-    return false;
-     // Ritorno false solo dopo aver controllato tutto
-}
-
-function checkUpper($password) {
-    for ($i = 0; $i < strlen($password); $i++) {
-        if (ctype_upper($password[$i])) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function checkSpecial($password) {
-    $specialChars = ['!', '?', '#', '&', '%'];
-    for ($i = 0; $i < strlen($password); $i++) {
-        if (in_array($password[$i], $specialChars)) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function validatePassword($password) {
-    // Controllo le regole e stampo quale manca
-    if (!checkLenght($password)) {
-        echo "Errore: La password deve avere almeno 8 caratteri.\n";
-        return false;
-    }
-    if (!checkNumber($password)) {
-        echo "Errore: La password deve contenere almeno un numero.\n";
-        return false;
-    }
-    if (!checkUpper($password)) {
-        echo "Errore: La password deve contenere almeno una maiuscola.\n";
-        return false;
-    }
-    if (!checkSpecial($password)) {
-        echo "Errore: La password deve contenere un carattere speciale (!, ?, #, &, %).\n";
-        return false;
-    }
-    
-    echo "Password corretta!\n";
-    return true;
 }
--- psw.php
+++ psw.php
@@ -1,11 +1,12 @@
 <?php
 //collegamento esterno-altro file-per la SEO
+// questo file richiede la psw dal file primario
 require('index.php');
 
 //uso del do while
 
 do{
-    
+    //chiedo la password qui
      $password = readLine("Inserisci la psw: ");
-  
+  // ed il controllo se ha la validità;
 }while (!result($password));
